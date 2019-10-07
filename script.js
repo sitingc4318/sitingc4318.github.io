dcc7a56ef42efbd4d5e47fdbfe77f38021c89608
sitingc4318 <56263464+sitingc4318@users.noreply.github.com>
2019-10-07 10:30:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "SiTingC."
+let userName = "sitingc4318";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
